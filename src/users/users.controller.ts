f46a23d8a7f3e7303573419d83aa9bdf95a2ab99
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-01-04 21:33:07 +0100

    setup findUser and createUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Patch,
+    Param,
+    Query,
+    Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
+    }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
-        console.log(body);
+        this.usersService.create(body.email, body.password);
     }
 }
