33a7d4753cb6acdc840f88f2c3e369691b17c5a5
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-01-12 22:12:22 +0100

    add session handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
     Delete,
     Post,
     NotFoundException,
+    Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -33,13 +34,22 @@ export class UsersController {
     }
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signout(@Session() session: any) {
+        session.userId = null;
     }
 
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Patch('/:id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,9 @@ export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
     findOne(id: number) {
+        if (!id) {
+            return null;
+        }
         return this.repo.findOneBy({ id });
     }
 
