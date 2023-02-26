d09595e7846e032daddc2c6ab03b2970e545e7db
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-02-26 17:12:02 +0100

    added elephantdb config

---
+++ elephantsql.js
@@ -0,0 +1,16 @@
+import { Client } from 'pg';
+
+var conString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mel.db.elephantsql.com/savujtkk`;
+var client = new Client(conString);
+client.connect(function (err) {
+    if (err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+        if (err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        client.end();
+    });
+});
