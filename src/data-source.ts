ee4ffffbb2bb52f5914a9b6bb502e3a84ab66ae8
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-02-27 09:25:39 +0100

    change datasource config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,11 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const appDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'db.sqlite',
+    type: process.env.DB_TYPE,
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
     entities: ['**/*.entity.ts'],
     migrations: [__dirname + '/migrations/*.ts'],
 } as DataSourceOptions);
